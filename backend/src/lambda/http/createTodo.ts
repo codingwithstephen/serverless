41d7627de4ec1ff55369bf684da83ba70017ce89
devlegend <stephen.peter.mooney@gmail.com>
2023-04-24 11:15:08 +0100

    create todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -3,19 +3,52 @@ import 'source-map-support/register'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+const AWS = require('aws-sdk');
+import * as uuid from 'uuid'
 
+const todoTable = process.env.TODOS_TABLE;
+
+const docClient = new AWS.DynamoDB.DocumentClient();
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
     // TODO: Implement creating a new TODO item
 
-    return undefined
-)
+    const userId = getUserId(event)
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    const todo = {
+      userId,
+        todoId: uuid.v4(),
+      done: false,
+      createdAt: new Date().toISOString(),
+    ...newTodo
+    }
+
+    await docClient.put({
+      TableName: todoTable,
+      Item: {
+        ...todo
+      }
+    }).promise()
+
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        todo
+      })
+    }
+  }
+);
 
 handler.use(
   cors({
     credentials: true
   })
-)
+);
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,20 +3,29 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+  signatureVersion: 'v4' // Use Sigv4 algorithm
+});
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION;
+const bucketName = process.env.IMAGES_S3_BUCKET;
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
 
-    return undefined
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+    return getSignedUrl(todoId);
   }
 )
-
+function getSignedUrl(todoId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
+}
 handler
   .use(httpErrorHandler())
   .use(
