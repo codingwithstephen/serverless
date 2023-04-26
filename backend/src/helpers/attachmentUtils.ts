5b40cea1155539c0c2d9c86d990ef4e6498f40ae
devlegend <stephen.peter.mooney@gmail.com>
2023-04-26 09:47:46 +0100

    additional classes

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -4,3 +4,20 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+
+async function uploadFileToS3() {
+  const s3 = new AWS.S3();
+
+  const params = {
+    Bucket: 'my-bucket',
+    Key: 'my-file.txt',
+    Body: 'Hello World!'
+  };
+
+  try {
+    const data = await s3.upload(params).promise();
+    console.log('File uploaded successfully:', data.Location);
+  } catch (err) {
+    console.log('Error uploading file:', err);
+  }
+}
