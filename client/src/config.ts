3a436995ef65af08e56fef19eed9a096f3fdaac2
devlegend <stephen.peter.mooney@gmail.com>
2023-05-01 20:02:03 +0100

    latest

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: 'dev-3tzvmvrlbquk2zkk.us.auth0.com',            // Auth0 domain
-  clientId: 'afOnpLtbySpxpnm1L9cURRi4auyNVgLz',          // Auth0 client id
+  domain: 'dev-3tzvmvrlbquk2zkk.us.auth0.com',          
+  clientId: 'afOnpLtbySpxpnm1L9cURRi4auyNVgLz',          
   callbackUrl: 'http://localhost:3000/callback'
 }
